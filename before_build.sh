5bf09f0b4694e018164e530c588cc19ea7e85e77
YnSen <65432445+YnSen@users.noreply.github.com>
2021-06-16 14:14:19 +0800

    Update before_build.sh

--- before_build.sh
+++ before_build.sh
@@ -7,8 +7,13 @@ git clone https://github.com/esirplayground/luci-app-poweroff
 git clone https://github.com/sirpdboy/luci-app-autopoweroff
 #腾讯ddns
 git clone https://github.com/Tencent-Cloud-Plugins/tencentcloud-openwrt-plugin-ddns
-pushd tencentcloud-openwrt-plugin-ddns/tencentcloud_ddns/files/luci/controller && popd
+pushd tencentcloud-openwrt-plugin-ddns/tencentcloud_ddns/files/luci/controller
+sed -i 's/"admin", "tencentcloud"/"admin", "network", "tencentcloud"/g' tencentddns.lua
 popd
+popd
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+sed -i 's/bootstrap/argon/g' feeds/luci/collections/luci/Makefile
 # Enter your commands here, e.g.
 # echo "Start build!"
 make defconfig
