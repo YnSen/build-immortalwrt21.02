c6b93dce73ef5fc0c1d9c7695b6bd683ed52f6b6
YnSen <65432445+YnSen@users.noreply.github.com>
2021-06-16 14:04:52 +0800

    Update before_build.sh

--- before_build.sh
+++ before_build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 cd /home/runner/immortalwrt
-
+pushd package
+#强制关机插件
+git clone https://github.com/esirplayground/luci-app-poweroff
+#自动关机插件
+git clone https://github.com/sirpdboy/luci-app-autopoweroff
+#腾讯ddns
+git clone https://github.com/Tencent-Cloud-Plugins/tencentcloud-openwrt-plugin-ddns
+pushd tencentcloud-openwrt-plugin-ddns/tencentcloud_ddns/files/luci/controller && popd
+popd
 # Enter your commands here, e.g.
 # echo "Start build!"
 make defconfig
